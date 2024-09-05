f98973fae47aec6bfd50636b4b26f27a0c5b5313
T1NT4 <thiagogabriel.osil@gmail.com>
2024-09-05 18:47:13 -0300

    fiz tudinhoo

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log('Hello World!');
+let sect = document.getElementById("resultados-pesquisa");
+let barra_pesquisa = document.getElementById("barra_pesquisa");
+
+function pesquisar(){
+    let resultados = "";
+    for(let dado of dados){
+        resultados += `
+            <div class="item-resultado">
+                <div>
+                <h2><a href="${dado.link}" target="_blank">${dado.titulo}</a></h2>
+                <p class="descricao-meta">
+                    ${dado.descricao}
+                </p>
+                <a href="${dado.link}" target="_blank">Comprar Jogo</a>
+                </div>
+                <figure>
+                    <a target="_blank" href="${dado.link}">
+                        <img src="${dado.img}" alt="">
+                    </a>
+                </figure>
+            </div>
+        `;
+    }
+    sect.innerHTML = resultados;
+}
---
+++ dados.js
@@ -0,0 +1,32 @@
+let dados = [
+    {
+        titulo: "Hollow Knight",
+        descricao: "Hollow Knight é um jogo de metroidvania com uma história emocionante. Explore o reino de Hallownest, derrote diversos inimigos e desvende os mistérios deste mundo intrigante.",
+        link: "https://store.steampowered.com/app/367520/Hollow_Knight/",
+        img: "imgs/Hollow_Knight_first_cover_art.webp.png"
+    },
+    {
+        titulo: "Outer Wilds",
+        descricao: "Explore um sistema solar repleto de maravilhas e perigos. Desvende os mistérios de uma civilização perdida e enfrente os desafios de um cosmos imprevisível. Mergulhe em um mundo aberto onde cada descoberta te leva mais perto de entender os segredos do universo.",
+        link: "https://store.steampowered.com/app/753640/Outer_Wilds/",
+        img: "imgs/Outer_Wilds_Steam_artwork.jpg"
+    },
+    {
+        titulo: "The Binding of Isaac: Rebirth",
+        descricao: "Um roguelike bizarro e viciante, onde você controla Isaac em sua jornada através de masmorras geradas proceduralmente, enfrentando monstros grotescos e descobrindo itens poderosos.",
+        link: "https://store.steampowered.com/app/250900/The_Binding_of_Isaac_Rebirth/",
+        img: "imgs/The_Binding_of_Issac_Rebirth_cover.jpg"
+    },
+    {
+        titulo: "ULTRAKILL",
+        descricao: "ULTRAKILL é um frenético jogo de tiro em primeira pessoa que combina ação rápida e combate brutal em um estilo retrô. Enfrente hordas de inimigos em uma série de ambientes dinâmicos e destrutíveis, com uma jogabilidade que homenageia os clássicos do gênero.",
+        link: "https://store.steampowered.com/app/1229490/ULTRAKILL/",
+        img: "imgs/ULTRAKILL_cover.png"
+    },
+    {
+        titulo: "Rain World",
+        descricao: "Rain World é um jogo de sobrevivência em uma selva pós-apocalíptica repleta de perigos. Jogue como um pequeno predador tentando sobreviver em um mundo hostil, onde a chuva implacável e a fauna agressiva oferecem constantes desafios.",
+        link: "https://store.steampowered.com/app/312520/Rain_World/",
+        img: "imgs/rain_world_cover.jpg"
+    }
+];
