9de3cf68c169ec641c31e996d1ab49f504409095
T1NT4 <thiagogabriel.osil@gmail.com>
2024-09-08 02:00:59 -0300

    cooler buttons

--- enterDetect.js
+++ enterDetect.js
@@ -2,5 +2,9 @@ let textInput = document.getElementById("campo_pesquisa");
 textInput.addEventListener('keydown', (event) => {
   if (event.key === 'Enter') {
     pesquisar()
+    document.getElementsByTagName("button")[0].classList.add('activated');
+    setTimeout(function(){
+        document.getElementsByTagName("button")[0].classList.remove('activated');
+    }, 250);
   }
 });
